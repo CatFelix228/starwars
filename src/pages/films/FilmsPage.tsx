ab875dbaf8cacda2a1932d9b86dc222f981e9286
Beerlogich <beerlogich@gmail.com>
2026-05-20 19:39:42 -0400

    123

--- src/pages/films/FilmsPage.tsx
+++ src/pages/films/FilmsPage.tsx
@@ -1,12 +1,16 @@
 import { useEffect } from "react";
-import { FilmType } from "../../types/entities";
 import styles from "./styles/filmsPage.module.scss";
+
 import FilmCard from "../../components/ui/film/FilmCard";
+
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/swiper-bundle.css";
+
 import { useAppDispatch, useAppSelector } from "../../redux-hooks";
+
 import { fetchAllFilms } from "../../features/Film/filmSlice";
+
 import { Circles } from "react-loader-spinner";
 
 const breakpoints = {
@@ -14,6 +18,7 @@ const breakpoints = {
     slidesPerView: 2,
     spaceBetween: 20,
   },
+
   1000: {
     slidesPerView: 3,
     spaceBetween: 30,
@@ -24,12 +29,12 @@ const FilmsPage: React.FC = () => {
   const dispatch = useAppDispatch();
 
   const films = useAppSelector((state) => state.films.list);
+
   const status = useAppSelector((state) => state.films.status);
-  console.log(films);
 
   useEffect(() => {
     dispatch(fetchAllFilms(""));
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className={styles.wrapper}>
@@ -39,23 +44,23 @@ const FilmsPage: React.FC = () => {
           width="80"
           color="#4fa94d"
           ariaLabel="circles-loading"
-          wrapperStyle={{}}
-          wrapperClass=""
           visible={true}
         />
       )}
+
       {status === "completed" && (
         <Swiper className={styles.swiper} breakpoints={breakpoints}>
-          {films?.map((film: FilmType) => {
-            return (
-              <SwiperSlide
-                key={film.properties?.episode_id}
-                style={{ display: "flex", justifyContent: "center" }}
-              >
-                <FilmCard {...film.properties} />
-              </SwiperSlide>
-            );
-          })}
+          {films.map((film) => (
+            <SwiperSlide
+              key={film.episode_id}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <FilmCard {...film} />
+            </SwiperSlide>
+          ))}
         </Swiper>
       )}
     </div>
