6e8eedbe6fb0752fc66eb1bb0ce46576bda17ed9
Beerlogich <beerlogich@gmail.com>
2026-05-20 19:54:19 -0400

    123

--- src/pages/films/FilmsPage.tsx
+++ src/pages/films/FilmsPage.tsx
@@ -1,16 +1,12 @@
 import { useEffect } from "react";
+import { FilmType } from "../../types/entities";
 import styles from "./styles/filmsPage.module.scss";
-
 import FilmCard from "../../components/ui/film/FilmCard";
-
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/swiper-bundle.css";
-
 import { useAppDispatch, useAppSelector } from "../../redux-hooks";
-
 import { fetchAllFilms } from "../../features/Film/filmSlice";
-
 import { Circles } from "react-loader-spinner";
 
 const breakpoints = {
@@ -18,7 +14,6 @@ const breakpoints = {
     slidesPerView: 2,
     spaceBetween: 20,
   },
-
   1000: {
     slidesPerView: 3,
     spaceBetween: 30,
@@ -29,7 +24,6 @@ const FilmsPage: React.FC = () => {
   const dispatch = useAppDispatch();
 
   const films = useAppSelector((state) => state.films.list);
-
   const status = useAppSelector((state) => state.films.status);
 
   useEffect(() => {
@@ -50,17 +44,18 @@ const FilmsPage: React.FC = () => {
 
       {status === "completed" && (
         <Swiper className={styles.swiper} breakpoints={breakpoints}>
-          {films.map((film) => (
-            <SwiperSlide
-              key={film.episode_id}
-              style={{
-                display: "flex",
-                justifyContent: "center",
-              }}
-            >
-              <FilmCard {...film} />
-            </SwiperSlide>
-          ))}
+          {films?.map((film: any) => {
+            const normalizedFilm: FilmType = film.properties ?? film;
+
+            return (
+              <SwiperSlide
+                key={normalizedFilm.episode_id}
+                style={{ display: "flex", justifyContent: "center" }}
+              >
+                <FilmCard {...normalizedFilm} />
+              </SwiperSlide>
+            );
+          })}
         </Swiper>
       )}
     </div>
